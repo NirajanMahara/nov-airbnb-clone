07fe79597ef2759b87f7434ab02972fcb4223290
Nirajan Mahara <mnirajan9@gmail.com>
2021-03-22 08:58:34 +0545

    nov discord-clone

---
+++ airbnb-clone/src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>NINJA © 2020 Airbnb Clone! - No Rights Reserved</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+}
+
+export default Footer;
